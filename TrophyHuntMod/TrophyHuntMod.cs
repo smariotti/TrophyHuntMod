127a37d74ced3dd1a0959754d15565d1cb4d31d9
Steve Mariotti <smariotti@gmail.com>
2024-08-14 19:01:56 -0700

    Checking in today's work, got the thing working now d

--- TrophyHuntMod/TrophyHuntMod.cs
+++ TrophyHuntMod/TrophyHuntMod.cs
@@ -1,17 +1,17 @@
-﻿using BepInEx;
+using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
 
-namespace JotunnModStub
+namespace TrophyHuntMod
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
-    internal class JotunnModStub : BaseUnityPlugin
+    internal class TrophyHuntMod : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
-        public const string PluginName = "JotunnModStub";
+        public const string PluginGUID = "com.jotunn.TrophyHuntMod";
+        public const string PluginName = "TrophyHuntMod";
         public const string PluginVersion = "0.0.1";
         
         // Use this class to add your own localization to the game
@@ -21,10 +21,13 @@ namespace JotunnModStub
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
+            Jotunn.Logger.LogInfo("TrophyHuntMod has landed");
             
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
     }
 }
+
+
+
