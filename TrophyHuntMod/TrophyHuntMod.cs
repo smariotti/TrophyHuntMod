6b8d7a40d9c9588233c5cb55acf3d45a9f688f25
Steve Mariotti <smariotti@gmail.com>
2024-08-15 06:48:18 -0700

    Removed references to Jotunn, which is no longer required for this mod.

--- TrophyHuntMod/TrophyHuntMod.cs
+++ TrophyHuntMod/TrophyHuntMod.cs
@@ -1,7 +1,7 @@
 using BepInEx;
-using Jotunn.Entities;
-using Jotunn.Managers;
-using Jotunn.Utils;
+//using Jotunn.Entities;
+//using Jotunn.Managers;
+//using Jotunn.Utils;
 using HarmonyLib;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,18 +16,18 @@ using static System.Net.Mime.MediaTypeNames;
 namespace TrophyHuntMod
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
-    [BepInDependency(Jotunn.Main.ModGuid)]
+ //   [BepInDependency(Jotunn.Main.ModGuid)]
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class TrophyHuntMod : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.TrophyHuntMod";
+        public const string PluginGUID = "com.oathorse.TrophyHuntMod";
         public const string PluginName = "TrophyHuntMod";
         public const string PluginVersion = "0.0.1";
         private readonly Harmony harmony = new Harmony(PluginGUID);
 
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
-        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
+//        public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
 
         private void Awake()
         {
