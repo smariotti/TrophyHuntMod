79c4d54d68fdf2f8e7c0cb0173e0d268ab83b509
Steve Mariotti <smariotti@gmail.com>
2024-08-14 19:08:24 -0700

    Trying again

--- TrophyHuntMod/TrophyHuntMod.cs
+++ TrophyHuntMod/TrophyHuntMod.cs
@@ -2,6 +2,16 @@ using BepInEx;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using TMPro;
+using System;
+using System.Configuration;
+using System.Security.Policy;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TrophyHuntMod
 {
@@ -13,7 +23,8 @@ namespace TrophyHuntMod
         public const string PluginGUID = "com.jotunn.TrophyHuntMod";
         public const string PluginName = "TrophyHuntMod";
         public const string PluginVersion = "0.0.1";
-        
+        private readonly Harmony harmony = new Harmony(PluginGUID);
+
         // Use this class to add your own localization to the game
         // https://valheim-modding.github.io/Jotunn/tutorials/localization.html
         public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
@@ -21,13 +32,364 @@ namespace TrophyHuntMod
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("TrophyHuntMod has landed");
-            
+            Jotunn.Logger.LogWarning("TrophyHuntMod has landed");
+
+            // Patch with Harmony
+            harmony.PatchAll();
+
             // To learn more about Jotunn's features, go to
             // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
         }
+
+        public enum Biome
+        {
+            Meadows,
+            Forest,
+            Ocean,
+            Swamp,
+            Mountains,
+            Plains,
+            Mistlands,
+            Ashlands
+        };
+
+        public struct TrophyData
+        {
+            public TrophyData(string name, Biome biome, int value)
+            {
+                m_name = name;
+                m_biome = biome;
+                m_value = value;
+            }
+
+            public string m_name;
+            public Biome m_biome;
+            public int m_value;
+        }
+
+        static public TrophyData[] __m_trophyData = new TrophyData[]
+        {
+            new TrophyData("TrophyAbomination", Biome.Swamp, 20),
+            new TrophyData("TrophyAsksvin", Biome.Ashlands, 50),
+            new TrophyData("TrophyBlob", Biome.Swamp, 20),
+            new TrophyData("TrophyBoar", Biome.Meadows, 10),
+            new TrophyData("TrophyBonemass", Biome.Swamp, 80),
+            new TrophyData("TrophyBonemawSerpent", Biome.Ashlands, 50),
+            new TrophyData("TrophyCharredArcher", Biome.Ashlands, 50),
+            new TrophyData("TrophyCharredMage", Biome.Ashlands, 50),
+            new TrophyData("TrophyCharredMelee", Biome.Ashlands, 50),
+            new TrophyData("TrophyCultist", Biome.Mountains, 30),
+            new TrophyData("TrophyCultist_Hildir", Biome.Mountains, 30),
+            new TrophyData("TrophyDeathsquito", Biome.Plains, 30),
+            new TrophyData("TrophyDeer", Biome.Meadows, 10),
+            new TrophyData("TrophyDragonQueen", Biome.Mountains, 100),
+            new TrophyData("TrophyDraugr", Biome.Swamp, 20),
+            new TrophyData("TrophyDraugrElite", Biome.Swamp, 20),
+            new TrophyData("TrophyDraugrFem", Biome.Swamp, 20),
+            new TrophyData("TrophyDvergr", Biome.Mistlands, 40),
+            new TrophyData("TrophyEikthyr", Biome.Meadows, 50),
+            new TrophyData("TrophyFader", Biome.Ashlands, 1000),
+            new TrophyData("TrophyFallenValkyrie", Biome.Ashlands, 50),
+            new TrophyData("TrophyFenring", Biome.Mountains, 30),
+            new TrophyData("TrophyForestTroll", Biome.Forest, 20),
+            new TrophyData("TrophyFrostTroll", Biome.Forest, 20),
+            new TrophyData("TrophyGjall", Biome.Mistlands, 40),
+            new TrophyData("TrophyGoblin", Biome.Plains, 30),
+            new TrophyData("TrophyGoblinBrute", Biome.Plains, 30),
+            new TrophyData("TrophyGoblinBruteBrosBrute", Biome.Plains, 30),
+            new TrophyData("TrophyGoblinBruteBrosShaman", Biome.Plains, 30),
+            new TrophyData("TrophyGoblinKing", Biome.Plains, 100),
+            new TrophyData("TrophyGoblinShaman", Biome.Plains, 30),
+            new TrophyData("TrophyGreydwarf", Biome.Forest, 20),
+            new TrophyData("TrophyGreydwarfBrute", Biome.Forest, 20),
+            new TrophyData("TrophyGreydwarfShaman", Biome.Forest, 20),
+            new TrophyData("TrophyGrowth", Biome.Plains, 30),
+            new TrophyData("TrophyHare", Biome.Mistlands, 40),
+            new TrophyData("TrophyHatchling", Biome.Mountains, 30),
+            new TrophyData("TrophyLeech", Biome.Swamp, 20),
+            new TrophyData("TrophyLox", Biome.Plains, 30),
+            new TrophyData("TrophyMorgen", Biome.Ashlands, 50),
+            new TrophyData("TrophyNeck", Biome.Meadows, 10),
+            new TrophyData("TrophySeeker", Biome.Mistlands, 40),
+            new TrophyData("TrophySeekerBrute", Biome.Mistlands, 40),
+            new TrophyData("TrophySeekerQueen", Biome.Mistlands, 1000),
+            new TrophyData("TrophySerpent", Biome.Ocean, 25),
+            new TrophyData("TrophySGolem", Biome.Mountains, 30),
+            new TrophyData("TrophySkeleton", Biome.Forest, 20),
+            new TrophyData("TrophySkeletonHildir", Biome.Forest, 20),
+            new TrophyData("TrophySkeletonPoison", Biome.Forest, 20),
+            new TrophyData("TrophySurtling", Biome.Swamp, 20),
+            new TrophyData("TrophyTheElder", Biome.Forest, 50),
+            new TrophyData("TrophyTick", Biome.Mistlands, 40),
+            new TrophyData("TrophyUlv", Biome.Mountains, 30),
+            new TrophyData("TrophyVolture", Biome.Ashlands, 50),
+            new TrophyData("TrophyWolf", Biome.Mountains, 30),
+            new TrophyData("TrophyWraith", Biome.Swamp, 20)
+        };
+
+        static GameObject __m_scoreTextElement = null;
+        static List<string> __m_trophiesInObjectDB = new List<string>();
+        static List<GameObject> __m_iconList = new List<GameObject>();
+
+        [HarmonyPatch(typeof(Player), "Awake")]
+        public class Player_Awake_Patch
+        {
+            static void Postfix(Player __instance)
+            {
+                Jotunn.Logger.LogWarning("Player is Awake()!");
+
+                // Pull the list of trophies from the ObjectDB
+                //
+                __m_trophiesInObjectDB.Clear();
+                foreach (GameObject item in ObjectDB.m_instance.m_items)
+                {
+                    ItemDrop component = item.GetComponent<ItemDrop>();
+
+                    if (component != null && component.m_itemData.m_shared.m_itemType == ItemDrop.ItemData.ItemType.Trophy)
+                    {
+                        __m_trophiesInObjectDB.Add(component.gameObject.name);
+                    }
+                }
+                Jotunn.Logger.LogWarning($"{__m_trophiesInObjectDB.Count} trophies discovered");
+
+                if (__m_trophiesInObjectDB.Count != __m_trophyData.Length)
+                {
+                    Jotunn.Logger.LogError($"Discovered Trophies ({__m_trophiesInObjectDB.Count}) doesn't match the stored Trophy data ({__m_trophyData.Length}), this mod is out of date.");
+                }
+
+                // Sort the trophies by biome, score and name
+                Array.Sort<TrophyData>(__m_trophyData, (x, y) => x.m_biome.CompareTo(y.m_biome) * 100000 + x.m_value.CompareTo(y.m_value) * 10000 + x.m_name.CompareTo(y.m_name));
+                foreach (var t in __m_trophyData)
+                {
+                    Jotunn.Logger.LogWarning($"{t.m_biome.ToString()}, {t.m_name}, {t.m_value}");
+                }
+
+                BuildUIElements();
+            }
+
+            static void BuildUIElements()
+            {
+                if (Hud.instance.m_rootObject != null)
+                {
+                    Jotunn.Logger.LogWarning("TrophyHuntMod: Hud.instance.m_rootObject is valid");
+                }
+                else
+                {
+                    Jotunn.Logger.LogError("TrophyHuntMod: Hud.instance.m_rootObject is NOT valid");
+                }
+
+                Transform healthPanelTransform = Hud.instance.transform.Find("hudroot/healthpanel");
+
+                if (healthPanelTransform != null)
+                {
+                    Debug.LogWarning("Health panel transform found!");
+                }
+                else
+                {
+                    Debug.LogError("Health panel transform not found.");
+                }
+
+                //GameObject trophyTray = CreateTrophyTrayElement(healthPanelTransform);
+
+                CreateScoreTextElement(healthPanelTransform);
+
+                CreateTrophyIconElements(healthPanelTransform, __m_trophyData);
+            }
+            static GameObject CreateTrophyTrayElement(Transform parentTransform)
+            {
+                GameObject trophyTray = new GameObject("TrophyTray");
+
+                trophyTray.transform.SetParent(parentTransform);
+                RectTransform trophyTrayRectTransform = trophyTray.AddComponent<RectTransform>();
+                trophyTrayRectTransform.sizeDelta = new Vector2(1000, 70);
+                trophyTrayRectTransform.anchoredPosition = new Vector2(0, 0);
+
+                UnityEngine.UI.Image trayImage = trophyTray.AddComponent<UnityEngine.UI.Image>();
+                trayImage.color = new Color(128, 128, 128, 128);
+                trayImage.raycastTarget = false;
+
+                return trophyTray;
+            }
+
+
+            static void CreateScoreTextElement(Transform parentTransform)
+            {
+                Jotunn.Logger.LogWarning("TrophyHuntMod: CreateTextElement() called");
+
+                // Create a new GameObject for the text
+                __m_scoreTextElement = new GameObject("ScoreText");
+
+                // Set the parent to the HUD canvas
+                __m_scoreTextElement.transform.SetParent(parentTransform);
+
+                // Add RectTransform component for positioning
+                RectTransform rectTransform = __m_scoreTextElement.AddComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(200, 50); // Set size
+                rectTransform.anchoredPosition = new Vector2(-65, -140); // Set position
+
+                int scoreValue = 9999;
+
+                TMPro.TextMeshProUGUI tmText = __m_scoreTextElement.AddComponent<TMPro.TextMeshProUGUI>();
+
+                tmText.text = $"{scoreValue}";
+                tmText.fontSize = 28;
+                tmText.color = Color.yellow;
+                tmText.alignment = TextAlignmentOptions.Center;
+                tmText.raycastTarget = false;
+            }
+            static void CreateTrophyIconElements(Transform parentTransform, TrophyData[] trophies)
+            {
+                foreach (TrophyData trophy in trophies)
+                {
+                    Sprite trophySprite = GetTrophySprite(trophy.m_name);
+                    if (trophySprite == null)
+                    {
+                        //ACK
+                    }
+
+                    GameObject iconElement = CreateIconElement(parentTransform, trophySprite, trophy.m_name, __m_iconList.Count);
+
+                    __m_iconList.Add(iconElement);
+                }    
+            }
+
+            static Sprite GetTrophySprite(string trophyPrefabName)
+            {
+                Jotunn.Logger.LogWarning($"TrophyHuntMod: GetTrophySprite() called for {trophyPrefabName}");
+
+                // Ensure the ObjectDB is loaded
+                if (ObjectDB.instance == null)
+                {
+                    Jotunn.Logger.LogError("ObjectDB is not loaded.");
+                    return null;
+                }
+
+                // Find the prefab for the specified trophy
+                GameObject trophyPrefab = ObjectDB.instance.GetItemPrefab(trophyPrefabName);
+                if (trophyPrefab == null)
+                {
+                    Jotunn.Logger.LogError($"Trophy prefab '{trophyPrefabName}' not found.");
+                    return null;
+                }
+
+                // Extract the ItemDrop component and get the item's icon
+                ItemDrop itemDrop = trophyPrefab.GetComponent<ItemDrop>();
+                if (itemDrop != null)
+                {
+                    return itemDrop.m_itemData.m_shared.m_icons[0];
+                }
+
+                Jotunn.Logger.LogError($"ItemDrop component not found on prefab '{trophyPrefabName}'.");
+                return null;
+            }
+
+            static GameObject CreateIconElement(Transform parentTransform, Sprite iconSprite, string iconName, int index)
+            {
+                Jotunn.Logger.LogWarning("TrophyHuntMod: CreateIconElement() called");
+
+                // Create a new GameObject for the icon
+                GameObject iconElement = new GameObject(iconName);
+
+                // Set the parent to the HUD canvas
+                iconElement.transform.SetParent(parentTransform);
+
+                int iconSize = 33;
+                int iconBorderSize = 0;
+                int xOffset = -30;
+                int yOffset = -140;
+
+                // Add RectTransform component for positioning
+                RectTransform rectTransform = iconElement.AddComponent<RectTransform>();
+                rectTransform.sizeDelta = new Vector2(iconSize, iconSize); // Set size
+                rectTransform.anchoredPosition = new Vector2(xOffset + index * (iconSize + iconBorderSize), yOffset); // Set position
+ 
+                // Add an Image component
+                UnityEngine.UI.Image image = iconElement.AddComponent<UnityEngine.UI.Image>();
+                image.sprite = iconSprite;
+                image.color = Color.black;
+                image.raycastTarget = false;
+
+                return iconElement;
+            }
+        }
+
+        [HarmonyPatch(typeof(Player), "Update")]
+        public class Player_Update_Patch
+        {
+            static void Postfix(ObjectDB __instance)
+            {
+                // Player.Update() has already occurred, now build the list of Trophies we've discovered
+                List<string> discoveredTrophies = Player.m_localPlayer.m_trophies.ToList<string>();
+
+                int score = 0;
+                foreach (TrophyData td in __m_trophyData)
+                {
+                    foreach (string s in discoveredTrophies)
+                    {
+                        if (td.m_name == s)
+                        {
+                            // Add the value to our score
+                            score += td.m_value;
+                        
+                            // Find the UI element and bold it
+                            foreach (GameObject o in __m_iconList)
+                            {
+                                UnityEngine.UI.Image image = o.GetComponent<UnityEngine.UI.Image>();
+                                if (image != null)
+                                {
+                                    if (image.name == s)
+                                    {
+                                        image.color = Color.white;
+
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                        }
+                    }
+                }
+
+                __m_scoreTextElement.GetComponent<TMPro.TextMeshProUGUI>().text = score.ToString();
+            }
+        }
+    
+        [HarmonyPatch(typeof(Hud), "Awake")]
+        public class Hud_Awake_Patch
+        {
+            static void Postfix(Hud __instance)
+            {
+                // This code runs after the Hud's Awake method
+                Debug.Log("TrophyHuntMod: HUD has been instantiated!");
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), nameof(Player.AddTrophy), new [] {typeof(ItemDrop.ItemData)} )]
+    public static class Player_AddTrophy_Patch
+    {
+        public static void Postfix(Player __instance, ItemDrop.ItemData item)
+        {
+            var player = __instance;
+            if (player != null)
+            {
+                if (item != null)
+                {
+                    var name = item.m_shared.m_name;
+
+                    Jotunn.Logger.LogWarning(string.Format("TrophyHuntMod: Trophy added! '{0}'", name));
+
+                    foreach (var x in player.m_trophies)
+                    {
+                        Jotunn.Logger.LogWarning(x);
+                    }
+                }
+            }
+        }
     }
 }
+
 
 
 
